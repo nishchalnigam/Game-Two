6bd7e3a59a07067a8360471b29880741e7575dd6
Nishchal Nigam <nishchalnigam@gmail.com>
2019-03-10 20:11:04 -0400

    Check

---
+++ Assets/Script/Artform.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Artform : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        //Rotate around y axis
+        transform.Rotate(new Vector3(0f, 1f, 0f));
+	}
+}
---
+++ Assets/Script/Jump.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jump : MonoBehaviour {
+
+    private Rigidbody rb;
+    public int jumpScaler;
+    private bool isGrounded = true;
+
+	// Use this for initialization
+	void Start () {
+
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.AddForce(transform.up * jumpScaler);//Object Up
+            //rb.AddForce(Vector3.up);//World Up
+            isGrounded = false;
+        }
+	}
+
+    void OnCollisionEnter(Collision collision)
+    {
+        isGrounded = true;
+    }
+}
