8b0cf155627ffe25e709a9a57aeddd266d0d2bb2
Nishchal Nigam <nishchalnigam@gmail.com>
2019-03-12 23:17:31 -0400

    Second

---
+++ Assets/Script/Move.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Rigidbody rb = GetComponent<Rigidbody>();
+        if (Input.GetKey(KeyCode.A))
+            rb.AddForce(new Vector3(-10,0,0));
+        if (Input.GetKey(KeyCode.D))
+            rb.AddForce(Vector3.right * 10);
+        if (Input.GetKey(KeyCode.W))
+            rb.AddForce(Vector3.forward * 10);
+        if (Input.GetKey(KeyCode.S))
+            rb.AddForce(Vector3.back * 10);
+    }
+}
